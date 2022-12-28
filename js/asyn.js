5bdea26e929e9b7ff7f9c65dcaecc1a2953dc08b
Wint Phyusin Maung <sshonn687@gmail.com>
2022-12-28 13:07:29 +0630

    api xmlrequest

--- js/asyn.js
+++ js/asyn.js
@@ -5,3 +5,14 @@ setTimeout(() => {
 },1000);
 
 console.log('continue at the same time');
+
+const request = new XMLHttpRequest(); //request object
+
+request.addEventListener('readystatechange',() => {
+      console.log(request,request.readyState);
+});
+
+request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+
+
+request.send();
