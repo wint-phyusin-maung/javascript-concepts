7b9c8212640fa46645d13e924240754176fc9792
Wint Phyusin Maung <sshonn687@gmail.com>
2023-01-08 18:23:01 +0630

    opeations

---
+++ js/operations.js
@@ -0,0 +1,14 @@
+//Javascript is a loosely Typed language
+var a;
+console.log(typeof a);
+
+var i = 23;
+let j = 23;
+
+console.log(this.i);
+console.log(this.j);
+
+{
+    var gh  = 'hello';
+}
+console.log(gh);
