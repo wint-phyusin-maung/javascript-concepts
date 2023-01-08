ccc75211170555d1e48c45e2c49a7254475483dd
Wint Phyusin Maung <sshonn687@gmail.com>
2023-01-08 10:10:47 +0630

    expected result did not find

--- js/letvsvar.js
+++ js/letvsvar.js
@@ -23,3 +23,19 @@ if(1 === 1){
     console.log(item);//pencil
 }
 console.log(item);//pencil
+
+
+var myName = 'I, var,is a global variable';
+
+let myname = 'I, let, is a local scope variable';
+
+console.log(this.myName);
+console.log(this.myname);
+
+var x = 'global';
+let y = 'global';
+console.log(this.x); // expect result "global" but I got undefined
+console.log(this.y); // undefined
+
+
+
