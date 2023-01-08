e7e7540a99e68f46478af53cc9adcba6bc6c1a91
Wint Phyusin Maung <sshonn687@gmail.com>
2023-01-08 22:11:16 +0630

    swtich

--- js/operations.js
+++ js/operations.js
@@ -10,6 +10,37 @@
 
 for(let i = 0 ; i < 4 ; i++){
    for(let j = 0; j < 4 ; j++){
-    console.log('*');
+   // console.log('*');
    }
+}
+
+//Decision Constructs
+
+var sum = 10;
+var itemcost = 2;
+if(sum %  itemcost === 0){
+    console.log("This is truthy");
+}
+
+//if statment
+var mid = 25;
+var high = 50;
+var low = 1;
+var current = 13;
+var found = -1;
+if(current < mid){
+    mid = (current - low) / 2 ;
+}
+
+//switch statements
+var month = "January";
+switch(month){
+    case "January":
+        console.log('This month is your birthday month.');
+        break;
+    case "October":
+        console.log('This month is my birthday');
+        break;
+    default:
+        console.log('I do not know your birthday.');
 }
