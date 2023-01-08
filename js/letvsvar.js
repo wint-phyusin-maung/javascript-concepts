d653056930c34d5de838e24365cff59fb3345fbc
Wint Phyusin Maung <sshonn687@gmail.com>
2023-01-08 09:58:05 +0630

    letvsvar

---
+++ js/letvsvar.js
@@ -0,0 +1,25 @@
+let name = "Wint Phyusin Maung";
+
+if(1 === 1){
+    name = "Khin";
+    console.log(name);//khin
+}
+console.log(name);//khin
+
+//let 
+let item  = "pencil";
+
+if(1 === 1){
+    let item = 'books';
+    console.log(item);//books
+}
+console.log(item);//pencil
+
+//let 
+var stuff  = "pencil";
+
+if(1 === 1){
+    var stuff = 'books';
+    console.log(item);//pencil
+}
+console.log(item);//pencil
