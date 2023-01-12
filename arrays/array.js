f926a755e191e31b3d561c7d867fe9d4a3ab4190
Wint Phyusin Maung <sshonn687@gmail.com>
2023-01-12 23:49:03 +0630

    constructor array

--- arrays/array.js
+++ arrays/array.js
@@ -1,2 +1,10 @@
 var numbers = [2,2];
 console.log(numbers.length);
+
+//array constructor
+var num = new Array();
+console.log(num.length);
+
+//array define
+var alGeNum = new Array(2,5,9,9);
+console.log(alGeNum);
