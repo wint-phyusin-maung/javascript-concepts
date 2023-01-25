3e052be3afb5d241fc5b5c1eb99c23b4d9d77788
Shonn-Shonn <sshonn687@gmail.com>
2023-01-25 11:35:06 +0630

    guessing elements[?]

--- arrays/addelement.js
+++ arrays/addelement.js
@@ -8,13 +8,24 @@ for(let i = 0 ; i < 30 ; i++)// to get the number which can be divided by 8.
        {
           if(array[i] % 8 == 0)
           {
-              console.log(array[i]);
+              //console.log(array[i]);
           }
        }
    }
 }
 
 //can use [] operator to get the element in the array
-let difElements = ['Khin Khin',true,2,0.235];
-console.log(difElements[1]);
-console.log(difElements[3]);
+let difElements = [1,2,3,4,5,6,7,8,9,10];
+// console.log(difElements[1]);
+// console.log(difElements[3]);
+
+
+
+//to sum all the element that can have in the array
+for(let i = 0 ; i < difElements.length ; i++)
+{
+    difElements[i] = i;
+    console.log(difElements[i]);
+}
+
+console.log(difElements);
