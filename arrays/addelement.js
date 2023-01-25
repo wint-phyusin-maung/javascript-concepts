fc367f48a9afc69d6283edfe066bb1489a2cd418
Shonn-Shonn <sshonn687@gmail.com>
2023-01-25 11:27:38 +0630

    [] operator

--- arrays/addelement.js
+++ arrays/addelement.js
@@ -13,3 +13,8 @@ for(let i = 0 ; i < 30 ; i++)// to get the number which can be divided by 8.
        }
    }
 }
+
+//can use [] operator to get the element in the array
+let difElements = ['Khin Khin',true,2,0.235];
+console.log(difElements[1]);
+console.log(difElements[3]);
