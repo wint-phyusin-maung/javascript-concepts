553be787e7b2679848bfa63116d41b9431ddae6a
Shonn-Shonn <sshonn687@gmail.com>
2023-01-25 13:59:00 +0630

    not?

---
+++ arrays/accessorfunction.js
@@ -0,0 +1,10 @@
+//Accessor fuctions
+var names = ["Khin","Hla","Aung","Kyaw",'Min'];
+var name = "Hla"; 
+var position = names.indexOf(name);
+if(position >= 0)
+{
+    console.log("Found " + name + " at position " + position)
+}else{
+    console.log(name + " not found in array.");
+}
