0288840b8ade23631f8bb140c529e78a57fe201c
Shonn-Shonn <sshonn687@gmail.com>
2023-01-28 23:23:23 +0630

    iterate even

--- arrays/iteratorfunction.js
+++ arrays/iteratorfunction.js
@@ -3,5 +3,16 @@ function square(num)
     console.log(num,num + num);
 }
 
+function even(num){
+    return num % 2 == 0;
+}
+
 var nums = [1,2,3,4,5];
-nums.forEach(square);
+nums.every(even);
+if(even)
+{
+    console.log("all numbers are even");
+}else{
+
+    console.log("not all numbers are even");
+}
