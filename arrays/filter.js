75032a28f1e9a99354fb364f5e4f4b907146b524
Shonn-Shonn <sshonn687@gmail.com>
2023-01-30 00:05:11 +0630

    two dimensional array

--- arrays/filter.js
+++ arrays/filter.js
@@ -38,6 +38,28 @@ var passGrades = grades.filter(passing);
 console.log(passGrades);
 
 
+//Creating two-dimensional arrays
+//rows and columns 
+
+
+Array.matrix = function(numrows,numcols,initial){
+    var arr = [];
+    for(var i = 0 ; i < numrows; i++){
+        var columns = [];
+        for(var j = 0 ; j < numcols ; j++){
+            columns[j] = initial;
+        }
+        arr[i] = columns;
+    }
+    return arr;
+}
+
+
+var nums = Array.matrix(5,5,0);
+console.log(nums[1][1]);
+var names = Array.matrix(3,3,"");
+names[1][2] = "Joe";
+console.log(names[1][2]);
 
 
 
