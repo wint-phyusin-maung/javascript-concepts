6117aa9805c5d5d27bae531efe7206dc7777ec0f
Shonn-Shonn <sshonn687@gmail.com>
2023-01-29 23:58:49 +0630

    fiter exam

--- arrays/filter.js
+++ arrays/filter.js
@@ -22,3 +22,26 @@ console.log(evens);
 var odds = nums.filter(isOdd);
 console.log(odds);
 
+
+function passing(num)
+{
+    return num >= 60;
+}
+
+var grades = [];
+for(var i = 0 ; i < 20 ; i++){
+    grades[i] = Math.floor(Math.random() * 101);
+}
+
+
+var passGrades = grades.filter(passing);
+console.log(passGrades);
+
+
+
+
+
+
+
+
+
