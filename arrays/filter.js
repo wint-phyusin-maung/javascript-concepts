e5ce942131f922e52cbd542b30aeca56d1f01a1e
Shonn-Shonn <sshonn687@gmail.com>
2023-01-29 23:55:27 +0630

    filter usage

---
+++ arrays/filter.js
@@ -0,0 +1,24 @@
+//filter
+
+function isEven(num)
+{
+    return num % 2 === 0;
+}   
+
+function isOdd(num)
+{
+    return num % 2 != 0;
+}
+
+var nums = [];
+for (var i = 0 ; i < 20 ; i++)
+{
+    nums[i] = i + 1;
+}
+
+var evens = nums.filter(isEven);
+console.log(evens);
+
+var odds = nums.filter(isOdd);
+console.log(odds);
+
