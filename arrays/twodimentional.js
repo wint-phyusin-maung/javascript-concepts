2fa9c8bad565bc44861458b842f0f994833284f3
Shonn-Shonn <sshonn687@gmail.com>
2023-01-31 10:33:26 +0630

    add twodimentional

--- arrays/twodimentional.js
+++ arrays/twodimentional.js
@@ -1,5 +1,10 @@
-var grades = [[98,76,73],[80,75,96],[65,76,55]];
+var grades = [[98,76,73,78],[80,75,96],[65,76,55],[89,76,90]];
+var total = 0;
 for(let i = 0 ; i < grades.length ; i++)
 {
     console.log(grades[i]);
+    for(let j = 0 ; j < grades[i].length; j++)
+    {
+       console.log(grades[i][j]);
+    }
 }
